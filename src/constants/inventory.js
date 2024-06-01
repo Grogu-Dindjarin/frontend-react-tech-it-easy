79f8068c934fe6e0c52b9890e053b515814b1113
Grogu-DindjarinGrogu-DindjarinGrogu-Dindjarin <grogu.dindjarin9@gmail.com>
2024-06-01 23:20:26 +0200

    Modified inventory constants
    Added Part 2 Array methods practise assignments

--- src/constants/inventory.js
+++ src/constants/inventory.js
@@ -1,331 +1,396 @@
 // BEST VERKOPENDE TV
 export const bestSellingTv = {
-    type: 'UHD 55AU7040',
-    name: 'Crystal',
-    brand: 'Samsung',
-    price: 549,
-    availableSizes: [43, 50, 55, 65],
-    refreshRate: 50,
-    screenType: 'LED-LCD',
-    screenQuality: 'Ultra HD/4K',
-    smartTv: true,
-    sourceImg: 'https://image.coolblue.nl/max/500x500/products/1786196',
-    options: [
-        {
-            name: "wifi",
-            applicable: true
-        },
-        {
-            name: "speech",
-            applicable: false
-        },
-        {
-            name: "hdr",
-            applicable: true
-        },
-        {
-            name: "bluetooth",
-            applicable: true
-        },
-        {
-            name: "ambiLight",
-            applicable: false
-        }
-    ],
-    originalStock: 52,
-    sold: 17,
-}
+  type: "UHD 55AU7040",
+  name: "Crystal",
+  brand: "Samsung",
+  price: 549,
+  availableSizes: [43, 50, 55, 65],
+  refreshRate: 50,
+  screenType: "LED-LCD",
+  screenQuality: "Ultra HD/4K",
+  smartTv: true,
+  sourceImg: "https://image.coolblue.nl/max/500x500/products/1786196",
+  options: [
+    {
+      name: "wifi",
+      applicable: true,
+    },
+    {
+      name: "speech",
+      applicable: false,
+    },
+    {
+      name: "hdr",
+      applicable: true,
+    },
+    {
+      name: "bluetooth",
+      applicable: true,
+    },
+    {
+      name: "ambiLight",
+      applicable: false,
+    },
+  ],
+  originalStock: 52,
+  sold: 17,
+};
 
 // STATISCHE VOORRAAD TV'S
 export let inventory = [
-    {
-        type: '43PUS6504/12',
-        name: '4K TV',
-        brand: 'Philips',
-        price: 379,
-        availableSizes: [43, 50, 58, 65],
-        refreshRate: 50,
-        screenType: 'LED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        sourceImg: 'https://media.s-bol.com/71r1kjwlLkWO/1200x793.jpg',
-        options: [
-            {
-                name: "wifi",
-                applicable: true,
-            },
-            {
-                name: "speech",
-                applicable: false,
-            },
-            {
-                name: "hdr",
-                applicable: true,
-            },
-            {
-                name: "bluetooth",
-                applicable: false,
-            },
-            {
-                name: "ambiLight",
-                applicable: false,
-            }
-        ],
-        originalStock: 23,
-        sold: 2,
-    },
-    {
-        type: 'NH3216SMART',
-        name: 'HD smart TV',
-        brand: 'Nikkei',
-        price: 159,
-        availableSizes: [32],
-        refreshRate: 100,
-        screenType: 'LED',
-        screenQuality: 'HD ready',
-        smartTv: true,
-        sourceImg: 'https://media.s-bol.com/NXrQRlwvDMkL/08R4V33/1200x764.jpg',
-        options: [
-            {
-                name: "wifi",
-                applicable: true,
-            },
-            {
-                name: "speech",
-                applicable: false,
-            },
-            {
-                name: "hdr",
-                applicable: false,
-            },
-            {
-                name: "bluetooth",
-                applicable: false,
-            },
-            {
-                name: "ambiLight",
-                applicable: false,
-            }
-        ],
-        originalStock: 4,
-        sold: 4,
-    },
-    {
-        type: 'QE55Q60T',
-        name: '4K QLED TV',
-        brand: 'Samsung',
-        price: 709,
-        availableSizes: [43, 50, 55, 58, 65],
-        refreshRate: 60,
-        screenType: 'QLED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        sourceImg: 'https://media.s-bol.com/B9n73k76VG2N/vQYEKNn/1200x734.jpg',
-        options:     [
-            {
-                name: "wifi",
-                applicable: true,
-            },
-            {
-                name: "speech",
-                applicable: true,
-            },
-            {
-                name: "hdr",
-                applicable: true,
-            },
-            {
-                name: "bluetooth",
-                applicable: true,
-            },
-            {
-                name: "ambiLight",
-                applicable: false,
-            }
-        ],
-        originalStock: 7,
-        sold: 0,
-    },
-    {
-        type: '43HAK6152',
-        name: 'Ultra HD SMART TV',
-        brand: 'Hitachi',
-        price: 349,
-        availableSizes: [43, 50, 55, 58],
-        refreshRate: 60,
-        screenType: 'LCD',
-        screenQuality: 'Ultra HD/4K',
-        sourceImg: 'https://media.s-bol.com/ROVXLp4q9joq/1200x751.jpg',
-        smartTv: true,
-        options:     [
-            {
-                name: "wifi",
-                applicable: true,
-            },
-            {
-                name: "speech",
-                applicable: true,
-            },
-            {
-                name: "hdr",
-                applicable: true,
-            },
-            {
-                name: "bluetooth",
-                applicable: true,
-            },
-            {
-                name: "ambiLight",
-                applicable: false,
-            }
-        ],
-        originalStock: 5,
-        sold: 5,
-    },
-    {
-        type: '50PUS7304/12',
-        name: 'The One 4K TV',
-        brand: 'Philips',
-        price: 479,
-        availableSizes: [43, 50, 55, 58, 65, 70],
-        refreshRate: 50,
-        screenType: 'LED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        sourceImg: 'https://media.s-bol.com/BPwRO1oZO32W/1200x721.jpg',
-        options: [
-            {
-                name: "wifi",
-                applicable: true,
-            },
-            {
-                name: "speech",
-                applicable: true,
-            },
-            {
-                name: "hdr",
-                applicable: true,
-            },
-            {
-                name: "bluetooth",
-                applicable: true,
-            },
-            {
-                name: "ambiLight",
-                applicable: true,
-            }
-        ],
-        originalStock: 8,
-        sold: 3,
-    },
-    {
-        type: '55PUS7805',
-        name: '4K LED TV',
-        brand: 'Philips',
-        price: 689,
-        availableSizes: [50, 55],
-        refreshRate: 100,
-        screenType: 'LED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        sourceImg: 'https://media.s-bol.com/wKJvXVlmPD7z/1200x702.jpg',
-        options: [
-            {
-                name: "wifi",
-                applicable: true,
-            },
-            {
-                name: "speech",
-                applicable: false,
-            },
-            {
-                name: "hdr",
-                applicable: true,
-            },
-            {
-                name: "bluetooth",
-                applicable: false,
-            },
-            {
-                name: "ambiLight",
-                applicable: true,
-            }
-        ],
-        originalStock: 6,
-        sold: 3,
-    },
-    {
-        type: 'B2450HD',
-        name: 'LED TV',
-        brand: 'Brandt',
-        price: 109,
-        availableSizes: [24],
-        refreshRate: 60,
-        screenType: 'LED',
-        screenQuality: 'Full HD',
-        smartTv: false,
-        sourceImg: 'https://media.s-bol.com/BLOQPk8ArJWo/1200x840.jpg',
-        options: [
-            {
-                name: "wifi",
-                applicable: false,
-            },
-            {
-                name: "speech",
-                applicable: false,
-            },
-            {
-                name: "hdr",
-                applicable: false,
-            },
-            {
-                name: "bluetooth",
-                applicable: false,
-            },
-            {
-                name: "ambiLight",
-                applicable: false,
-            }
-        ],
-        originalStock: 10,
-        sold: 8,
-    },
-    {
-        type: '32WL1A63DG',
-        name: 'HD TV',
-        brand: 'Toshiba',
-        price: 161,
-        availableSizes: [32, 43, 50],
-        refreshRate: 50,
-        screenType: 'LED',
-        screenQuality: 'Full HD',
-        smartTv: false,
-        sourceImg: 'https://media.s-bol.com/ROVXLp4q9joq/1200x751.jpg',
-        options: [
-            {
-                name: "wifi",
-                applicable: false,
-            },
-            {
-                name: "speech",
-                applicable: false,
-            },
-            {
-                name: "hdr",
-                applicable: true,
-            },
-            {
-                name: "bluetooth",
-                applicable: false,
-            },
-            {
-                name: "ambiLight",
-                applicable: false,
-            }
-        ],
-        originalStock: 10,
-        sold: 8,
-    },
+  {
+    type: "43PUS6504/12",
+    name: "4K TV",
+    brand: "Philips",
+    price: 379,
+    availableSizes: [43, 50, 58, 65],
+    refreshRate: 50,
+    screenType: "LED",
+    screenQuality: "Ultra HD/4K",
+    smartTv: true,
+    sourceImg: "https://media.s-bol.com/71r1kjwlLkWO/1200x793.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: true,
+      },
+      {
+        name: "speech",
+        applicable: false,
+      },
+      {
+        name: "hdr",
+        applicable: true,
+      },
+      {
+        name: "bluetooth",
+        applicable: false,
+      },
+      {
+        name: "ambiLight",
+        applicable: false,
+      },
+    ],
+    originalStock: 23,
+    sold: 2,
+  },
+  {
+    type: "NH3216SMART",
+    name: "HD smart TV",
+    brand: "Nikkei",
+    price: 159,
+    availableSizes: [32],
+    refreshRate: 100,
+    screenType: "LED",
+    screenQuality: "HD ready",
+    smartTv: true,
+    sourceImg: "https://media.s-bol.com/NXrQRlwvDMkL/08R4V33/1200x764.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: true,
+      },
+      {
+        name: "speech",
+        applicable: false,
+      },
+      {
+        name: "hdr",
+        applicable: false,
+      },
+      {
+        name: "bluetooth",
+        applicable: false,
+      },
+      {
+        name: "ambiLight",
+        applicable: false,
+      },
+    ],
+    originalStock: 4,
+    sold: 4,
+  },
+  {
+    type: "QE55Q60T",
+    name: "4K QLED TV",
+    brand: "Samsung",
+    price: 709,
+    availableSizes: [43, 50, 55, 58, 65],
+    refreshRate: 60,
+    screenType: "QLED",
+    screenQuality: "Ultra HD/4K",
+    smartTv: true,
+    sourceImg: "https://media.s-bol.com/B9n73k76VG2N/vQYEKNn/1200x734.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: true,
+      },
+      {
+        name: "speech",
+        applicable: true,
+      },
+      {
+        name: "hdr",
+        applicable: true,
+      },
+      {
+        name: "bluetooth",
+        applicable: true,
+      },
+      {
+        name: "ambiLight",
+        applicable: false,
+      },
+    ],
+    originalStock: 7,
+    sold: 0,
+  },
+  {
+    type: "43HAK6152",
+    name: "Ultra HD SMART TV",
+    brand: "Hitachi",
+    price: 349,
+    availableSizes: [43, 50, 55, 58],
+    refreshRate: 60,
+    screenType: "LCD",
+    screenQuality: "Ultra HD/4K",
+    sourceImg: "https://media.s-bol.com/ROVXLp4q9joq/1200x751.jpg",
+    smartTv: true,
+    options: [
+      {
+        name: "wifi",
+        applicable: true,
+      },
+      {
+        name: "speech",
+        applicable: true,
+      },
+      {
+        name: "hdr",
+        applicable: true,
+      },
+      {
+        name: "bluetooth",
+        applicable: true,
+      },
+      {
+        name: "ambiLight",
+        applicable: false,
+      },
+    ],
+    originalStock: 5,
+    sold: 5,
+  },
+  {
+    type: "50PUS7304/12",
+    name: "The One 4K TV",
+    brand: "Philips",
+    price: 479,
+    availableSizes: [43, 50, 55, 58, 65, 70],
+    refreshRate: 50,
+    screenType: "LED",
+    screenQuality: "Ultra HD/4K",
+    smartTv: true,
+    sourceImg: "https://media.s-bol.com/BPwRO1oZO32W/1200x721.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: true,
+      },
+      {
+        name: "speech",
+        applicable: true,
+      },
+      {
+        name: "hdr",
+        applicable: true,
+      },
+      {
+        name: "bluetooth",
+        applicable: true,
+      },
+      {
+        name: "ambiLight",
+        applicable: true,
+      },
+    ],
+    originalStock: 8,
+    sold: 3,
+  },
+  {
+    type: "55PUS7805",
+    name: "4K LED TV",
+    brand: "Philips",
+    price: 689,
+    availableSizes: [50, 55],
+    refreshRate: 100,
+    screenType: "LED",
+    screenQuality: "Ultra HD/4K",
+    smartTv: true,
+    sourceImg: "https://media.s-bol.com/wKJvXVlmPD7z/1200x702.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: true,
+      },
+      {
+        name: "speech",
+        applicable: false,
+      },
+      {
+        name: "hdr",
+        applicable: true,
+      },
+      {
+        name: "bluetooth",
+        applicable: false,
+      },
+      {
+        name: "ambiLight",
+        applicable: true,
+      },
+    ],
+    originalStock: 6,
+    sold: 3,
+  },
+  {
+    type: "B2450HD",
+    name: "LED TV",
+    brand: "Brandt",
+    price: 109,
+    availableSizes: [24],
+    refreshRate: 60,
+    screenType: "LED",
+    screenQuality: "Full HD",
+    smartTv: false,
+    sourceImg: "https://media.s-bol.com/BLOQPk8ArJWo/1200x840.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: false,
+      },
+      {
+        name: "speech",
+        applicable: false,
+      },
+      {
+        name: "hdr",
+        applicable: false,
+      },
+      {
+        name: "bluetooth",
+        applicable: false,
+      },
+      {
+        name: "ambiLight",
+        applicable: false,
+      },
+    ],
+    originalStock: 10,
+    sold: 8,
+  },
+  {
+    type: "32WL1A63DG",
+    name: "HD TV",
+    brand: "Toshiba",
+    price: 161,
+    availableSizes: [32, 43, 50],
+    refreshRate: 50,
+    screenType: "LED",
+    screenQuality: "Full HD",
+    smartTv: false,
+    sourceImg: "https://media.s-bol.com/ROVXLp4q9joq/1200x751.jpg",
+    options: [
+      {
+        name: "wifi",
+        applicable: false,
+      },
+      {
+        name: "speech",
+        applicable: false,
+      },
+      {
+        name: "hdr",
+        applicable: true,
+      },
+      {
+        name: "bluetooth",
+        applicable: false,
+      },
+      {
+        name: "ambiLight",
+        applicable: false,
+      },
+    ],
+    originalStock: 10,
+    sold: 8,
+  },
 ];
 
+// Oefenen met array methodes
 
+// Opdracht 1a: Gebruik een array-methode om een array te maken met daarin alle tv-type namen. Log de uitkomst in de console.
+let tvTypeNames = inventory.map((tvTypeName) => {
+  return tvTypeName.name;
+});
+
+console.log("Deel 2 - Oefenen met array methodes - Opdracht 1a: ", tvTypeNames);
+
+// Opdracht 1b: Gebruik een array-methode om alle informatie te verzamelen van de tv's die volledig uitverkocht zijn. Log de uitkomst in de console.
+
+let soldOutTvs = inventory.filter((soldOutTv) => {
+  return soldOutTv.sold === soldOutTv.originalStock;
+});
+
+console.log("Deel 2 - Oefenen met array methodes - Opdracht 1b: ", soldOutTvs);
+
+// Opdracht 1c: Gebruik een array-methode om de informatie van het tv-type 'NH3216SMART' op te halen.
+
+let smartTv = inventory.find((smartTv) => {
+  return smartTv.type === "NH3216SMART";
+});
+
+console.log("Deel 2 - Oefenen met array methodes - Opdracht 1c: ", smartTv);
+
+// Opdracht 1d: Gebruik een array-methode om een lijst te maken van de merk- en tv-namen waarbij aangegeven wordt of deze geschikt zijn voor sport-kijkers (een verversingssnelheid van 100Hz of hoger). Doe dit in het format { name: 'Toshiba HD TV', suitable: false }. Log de uitkomst in de console.
+
+let suitableTvsForSport = inventory.map((suitableTvForSport) => {
+  return {
+    name: `${suitableTvForSport.brand} ${suitableTvForSport.name}`,
+    suitable: suitableTvForSport.refreshRate >= 100,
+  };
+});
+
+console.log(
+  "Deel 2 - Oefenen met array methodes - Opdracht 1d: ",
+  suitableTvsForSport,
+);
+
+// Opdracht 1e (uitdaging): Gebruik array-methoden om alle informatie te verzamelen van de tv's die beschikbaar zijn in schermgroottes van 65 inch en groter.
+
+let bigScreenTvsOf65InchAndHigher = inventory.filter(
+  (bigScreenTvOf65InchAndHigher) => {
+    return bigScreenTvOf65InchAndHigher.availableSizes.some(
+      (size) => size >= 65,
+    );
+  },
+);
+
+console.log(
+  "Deel 2 - Oefenen met array methodes - Opdracht 1e (uitdaging): ",
+  bigScreenTvsOf65InchAndHigher,
+);
+
+// Opdracht 1f (uitdaging): Gebruik array-methoden om alle informatie te verzamelen van de tv's die over ambilight beschikken. Log de uitkomst in de console.
+
+let tvsWithAmbilight = inventory.filter((tvWithAmbilight) => {
+  return tvWithAmbilight.options.find(
+    (option) => option.name === "ambiLight" && option.applicable === true,
+  );
+});
+
+console.log(
+  "Deel 2 - Oefenen met array methodes - Opdracht 1f (uitdaging): ",
+  tvsWithAmbilight,
+);
